e3171bb9f369eefbfc6f5880159a66f94bd6593e
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-02-12 13:43:40 +0000

    db編集

--- db/migrate/20231224111751_create_posts.rb
+++ db/migrate/20231224111751_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.integer :customer_id
+      t.string :name
+      t.text :sentence
+      t.integer :genre
       t.timestamps
     end
   end
