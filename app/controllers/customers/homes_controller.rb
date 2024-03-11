257ebd890f72b85b16394fa1ddc80667660ec57e
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-03-11 14:33:36 +0000

    会員側のコントローラー編集

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -1,7 +1,27 @@
 class Customers::HomesController < ApplicationController
   def top
+    @posts = Post.all
   end
 
   def about
+  end
+  
+  def search
+    search_word = params[:word]
+    search_count_word = params[:count_word]
+    search_genre = params[:genre]
+    genre_num = Post.genres[search_genre]
+    # 料理名の検索機能
+    # @posts = Post.where("name LIKE ? or sentence LIKE ? or genre = ?", "%#{search_word}%", "%#{search_count_word}%", genre_num)
+    @posts = Post.all
+    if params[:word]!=""
+      @posts = @posts.where("name LIKE ? or sentence LIKE ?", "%#{search_word}%", "%#{search_word}%")
+    end
+    if params[:count_word]!=""
+      @posts = @posts.where("sentence LIKE ?", "%#{search_count_word}%" )
+    end
+    if params[:genre]!=""
+      @posts = @posts.where("genre = ?", genre_num)
+    end
   end
 end
