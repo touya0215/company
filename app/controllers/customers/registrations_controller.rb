c64487f1561b7d5a7d53f2be261b6dc1d0d846ee
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-03-22 06:39:02 +0000

    ログインコントローラー編集

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -38,7 +38,11 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kama, :first_name_kana, :telephone_number])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -51,9 +55,10 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(resource)
   #   super(resource)
-  # end
+    customer_path(current_customer.id)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
