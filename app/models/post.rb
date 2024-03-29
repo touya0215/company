ec2f84ba9e41237d1c582d80b0b2dbd80be5cf33
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-03-29 14:46:22 +0000

    投稿モデル編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+  belongs_to :customer
+  has_many :likes, dependent: :destroy #投稿が削除されるといいねも削除される。
+  has_many :comments, dependent: :destroy #投稿が削除されるとコメントも削除される。
+
+  has_one_attached :image
+
+  enum genre: { noodles: 1, main_dish: 2, side_dish: 3, rice_bowls: 4, dessert: 5 }
+
+  def self.search(keyword)
+    # タグ名の検索機能
+    where("facility_name LIKE ?", "%#{keyword}%")
+  end
+
 end
