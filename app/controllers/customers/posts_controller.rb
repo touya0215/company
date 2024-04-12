373af72bb32da7a3f09d50e44143dc2bdb561c37
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-04-12 14:16:52 +0000

    投稿コントローラー編集

--- app/controllers/customers/posts_controller.rb
+++ app/controllers/customers/posts_controller.rb
@@ -1,13 +1,47 @@
 class Customers::PostsController < ApplicationController
+
+  def create
+    @post = current_customer.posts.new(post_params)
+    @post.save
+    redirect_to customers_homes_top_path
+  end
+
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments  #投稿詳細に関連付けてあるコメントを全取得
+    if customer_signed_in?
+      @comment = current_customer.comments.new  #投稿詳細画面でコメントの投稿を行うので、formのパラメータ用にCommentオブジェクトを取得
+    end
   end
 
   def edit
+    @posts = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to customers_homes_top_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:customer_id, :name, :image, :sentence, :genre)
+  end
+
 end
