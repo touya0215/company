842fbabf7b6b62d692e9f42fdb9d32a8786eb7ae
touya0215 <110541062+touya0215@users.noreply.github.com>
2024-04-23 15:14:33 +0000

    admin　投稿コントローラー編集

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -1,13 +1,29 @@
 class Admins::PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post[:id])
+  end
+
   def new
+    @post = Post.new
   end
 
   def index
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :sentence, :genre)
+  end
+
 end
